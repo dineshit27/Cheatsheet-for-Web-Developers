5e92280dcb06e77e777f65ab1f5024cc4ccd09ad
M. Dinesh <m.dinesh.it27@gmail.com>
2025-02-16 09:24:28 +0530

    Create deploy.sh

---
+++ Tailwindcss/deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# copy old version
+# cp -r ./v1 ./dist
+# cp -r ./v2.0 ./dist
+cp -r ./v2.1 ./dist
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo "Deploying....."
+echo "https://umeshmk.github.io/Tailwindcss-cheatsheet/" 
+# echo "https://umeshmk.github.io/Tailwindcss-cheatsheet/" > CNAME
+
+git init
+git add -A
+git commit -m "deploy" &&
+
+git push -f git@github.com:umeshmk/Tailwindcss-cheatsheet.git master:gh-pages
+
+cd -
